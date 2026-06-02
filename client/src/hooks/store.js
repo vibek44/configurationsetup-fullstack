9090ba85d350d95d39e69a25291bcda7139776d7
vibek44 <kshetrivibek@gmail.com>
2026-06-02 21:21:42 +0300

    implementing zustand to  manage application state and logic into own hook module and exporting useBlog,useNotification,useUser and action hook for use

---
+++ client/src/hooks/store.js
@@ -0,0 +1,116 @@
+import { create } from "zustand";
+import blogService from "../services/blogs";
+import loginService from "../services/login";
+
+const useNotificationStore = create((set, get) => ({
+  notification: {
+    success: null,
+    error: null,
+  },
+  actions: {
+    setError: (error = null) =>
+      set((state) => ({ notification: { ...state.notification, error } })),
+    setSuccess: (success = null) =>
+      set((state) => ({ notification: { ...state.notification, success } })),
+  },
+}));
+
+export const useNotification = () =>
+  useNotificationStore((state) => state.notification);
+export const useNotificationActions = () =>
+  useNotificationStore((state) => state.actions);
+
+const useUserStore = create((set, get) => ({
+  user: null,
+  actions: {
+    setUser: (user) => set(() => ({ user })),
+    handleLogin: async (userlog) => {
+      try {
+        const user = await loginService.login(userlog);
+        set(() => ({ user }));
+        blogService.setToken(user.token);
+        localStorage.setItem("userJson", JSON.stringify(user));
+        return user;
+      } catch (error) {
+        useNotificationStore
+          .getState()
+          .actions.setError(error?.response?.data?.error);
+        setTimeout(() => {
+          useNotificationStore.getState().actions.setError();
+        }, 3000);
+      }
+    },
+  },
+}));
+
+export const useUser = () => useUserStore((state) => state.user);
+export const useUserActions = () => useUserStore((state) => state.actions);
+
+const useBlogStore = create((set, get) => ({
+  blogs: [],
+  actions: {
+    initialize: async () => {
+      try {
+        const blogs = await blogService.getAll();
+        console.log(blogs);
+        set((state) => ({ blogs }));
+      } catch (error) {
+        set(() => ({ blogs: undefined }));
+      }
+    },
+    addBlog: async (blog) => {
+      try {
+        const savedBlog = await blogService.create(blog);
+        set((state) => ({ blogs: state.blogs.concat(savedBlog) }));
+        useNotificationStore
+          .getState()
+          .actions.setSuccess(
+            `a new blog ${savedBlog.title}! by ${savedBlog.author} added`
+          );
+        setTimeout(() => {
+          useNotificationStore.getState().actions.setSuccess();
+        }, 3000);
+      } catch (error) {
+        useNotificationStore
+          .getState()
+          .actions.setError(error?.response?.data?.error);
+        setTimeout(() => {
+          useNotificationStore.getState().actions.setError();
+        }, 3000);
+      }
+    },
+    handleLikeUpdate: async (blog) => {
+      try {
+        const updatedBlog = await blogService.update(blog);
+        set((state) => ({
+          blogs: state.blogs.map((ele) =>
+            ele.id !== updatedBlog.id ? ele : updatedBlog
+          ),
+        }));
+      } catch (error) {
+        useNotificationStore
+          .getState()
+          .actions.setError(error?.response?.data?.error);
+        setTimeout(() => {
+          useNotificationStore.getState().actions.setError();
+        }, 3000);
+      }
+    },
+    removeBlog: async (id) => {
+      try {
+        await blogService.remove(id);
+        set((state) => ({ blogs: state.blogs.filter((ele) => ele.id !== id) }));
+      } catch (error) {
+        useNotificationStore
+          .getState()
+          .actions.setError(error?.response?.data?.error);
+        setTimeout(() => {
+          useNotificationStore.getState().actions.setError();
+        }, 3000);
+      }
+    },
+  },
+}));
+
+export const useBlog = () => useBlogStore((state) => state.blogs);
+export const useBlogActions = () => useBlogStore((state) => state.actions);
