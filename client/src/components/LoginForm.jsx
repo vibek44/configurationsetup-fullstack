b6303602dc402e12340f5e05c0d9539d25eea2e3
vibek44 <kshetrivibek@gmail.com>
2026-06-03 15:26:58 +0300

    retrieving handleLogin,setError functions needed for LoginForm  using hook store instead of passing function as  props

--- client/src/components/LoginForm.jsx
+++ client/src/components/LoginForm.jsx
@@ -1,51 +1,64 @@
-import { TextField ,Button,Grid} from '@mui/material'
-import { useState } from 'react'
+import { TextField, Button, Grid } from "@mui/material";
+import { useState } from "react";
+import { useNotificationActions, useUserActions } from "../hooks/store";
+import { useNavigate } from "react-router-dom";
 
-const LoginForm=( { handleLogin }) => {
-  const [userName, setUserName]=useState('')
-  const [password, setPassword]=useState('')
+const LoginForm = () => {
+  const navigate = useNavigate();
+  const [userName, setUserName] = useState("");
+  const [password, setPassword] = useState("");
+  const { setError } = useNotificationActions();
+  const { handleLogin } = useUserActions();
 
-  const handleLoginFormInput  = (e) => {
-    e.preventDefault()
-    handleLogin({ userName,password })
-    setUserName('')
-    setPassword('')
-  }
+  const handleLoginFormInput = async (e) => {
+    e.preventDefault();
+    if (!userName || !password) {
+      setError("username or password missing");
+      setTimeout(() => {
+        setError();
+      }, 2000);
+      return;
+    }
+    const user = await handleLogin({ userName, password });
+    if (user) navigate("/");
+    setUserName("");
+    setPassword("");
+  };
 
-  return(
+  return (
     <div>
-       <h3>Log in to application</h3>
-      <form onSubmit={handleLoginFormInput} >
-        <Grid  >
-          <Grid >
-          <TextField
-            size='small'
-            variant='standard'
-            label="username"
-            type='text'
-            value={userName}
-            onChange={({ target }) => setUserName(target.value.trim())}
-          />
+      <h3>Log in to application</h3>
+      <form onSubmit={handleLoginFormInput}>
+        <Grid>
+          <Grid>
+            <TextField
+              size="small"
+              variant="standard"
+              label="username"
+              type="text"
+              value={userName}
+              onChange={({ target }) => setUserName(target.value.trim())}
+            />
           </Grid>
           <Grid>
-          <TextField 
-            size='small'
-            variant='standard'
-            label='password'
-            type='password'
-            value={password}
-            onChange={({ target }) => setPassword(target.value.trim())}
-          />
+            <TextField
+              size="small"
+              variant="standard"
+              label="password"
+              type="password"
+              value={password}
+              onChange={({ target }) => setPassword(target.value.trim())}
+            />
           </Grid>
           <Grid>
-            <Button type="submit" variant='contained' sx={{mt:3}} >login</Button>
-          </Grid>         
+            <Button type="submit" variant="contained" sx={{ mt: 3 }}>
+              login
+            </Button>
+          </Grid>
         </Grid>
       </form>
     </div>
-  )
-}
+  );
+};
 
-
-
-export default LoginForm
+export default LoginForm;
