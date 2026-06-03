e4cf369ac2a5d6ceff2c83c8139b6eddaa19ee90
vibek44 <kshetrivibek@gmail.com>
2026-06-03 15:33:39 +0300

    Refactoring the code in App file so that all component receives state and function from store instead of passing it as props

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, useRef } from "react";
+import { useEffect } from "react";
 import { Route, Routes, useMatch, useNavigate } from "react-router-dom";
 import { Container } from "@mui/material";
 import ErrorBoundary from "./components/ErrorBoundary";
@@ -10,137 +10,53 @@ import Blog from "./components/Blog";
 import Togglable from "./components/Togglable";
 import LoginForm from "./components/LoginForm";
 import BlogForm from "./components/BlogForm";
-import {
-  useNotificationActions,
-  useBlogActions,
-  useBlog,
-  useUserActions,
-  useUser,
-} from "./hooks/store";
+import { useBlogActions, useUserActions } from "./hooks/store";
 import blogService from "./services/blogs";
-
+import { getUser } from "./services/persistentUser";
 const App = () => {
-  const user = useUser();
-  //console.log(user);
-  const { setUser } = useUserActions();
-  const { handleLogin } = useUserActions();
-  const blogs = useBlog();
-  const { initialize, addBlog, handleLikeUpdate, removeBlog } =
-    useBlogActions();
-  const { setError, setSuccess } = useNotificationActions();
+  const { setUserStore } = useUserActions();
 
-  const blogFormRef = useRef();
-  const sortedBlogs = blogs
-    ? blogs.toSorted((a, b) => b.likes - a.likes)
-    : undefined;
+  const { initialize } = useBlogActions();
 
   const navigate = useNavigate();
-  const match = useMatch("/blogs/:id"); //match is object with property params
-  const blog =
-    sortedBlogs &&
-    sortedBlogs.find((el) => (match ? el.id === match.params.id : null));
+
+  const match = useMatch("/blogs/:id");
+
   useEffect(() => {
     initialize();
   }, []);
 
   useEffect(() => {
-    const userJson = localStorage.getItem("userJson");
-    if (userJson) {
-      const user = JSON.parse(userJson);
-      blogService.setToken(user.token);
-      setUser(user);
+    const userLocal = getUser();
+    if (userLocal) {
+      blogService.setToken(userLocal.token);
+      setUserStore(userLocal);
       if (match) {
         navigate(`/blogs/${match.params.id}`);
       } else navigate("/");
     }
   }, []);
 
-  const handleLoginForm = async ({ userName, password }) => {
-    if (!userName || !password) {
-      setError("username or password missing");
-      setTimeout(() => {
-        setError();
-      }, 2000);
-      return;
-    }
-    const user = await handleLogin({ userName, password });
-    if (user) navigate("/");
-  };
-
-  const handleBlogForm = async ({ title, author, url }) => {
-    if (!title || !author || !url) {
-      setError("title,author or url missing!");
-      setTimeout(() => {
-        setError();
-      }, 3000);
-      return;
-    }
-    blogFormRef.current.toggleVisibility();
-    await addBlog({ title, author, url });
-    navigate("/");
-  };
-
-  const handleBlogUpdate = async (changedBlog) => {
-    await handleLikeUpdate(changedBlog);
-  };
-
-  const handleRemove = async (removedBlog) => {
-    const result = confirm(
-      `Remove ${removedBlog.title} by ${removedBlog.author}`
-    );
-    if (!result) return;
-    await removeBlog(removedBlog.id);
-    navigate("/");
-  };
-
-  const handleLogout = () => {
-    setUser(null);
-    blogService.setToken("not-allowed");
-    localStorage.removeItem("userJson");
-    navigate("/");
-  };
-
   return (
     <Container disableGutters={false}>
-      <MenuLink
-        user={user}
-        handleLogout={handleLogout}
-        sortedBlogs={sortedBlogs}
-      />
+      <MenuLink />
       <Notification />
       <Routes>
         <Route
           path="/create"
           element={
-            <Togglable ref={blogFormRef} buttonLabel="create">
-              <BlogForm handleBlogForm={handleBlogForm} />
+            <Togglable buttonLabel="create">
+              <BlogForm />
             </Togglable>
           }
         />
-        <Route
-          path="/blogs/:id"
-          element={
-            blog ? (
-              <Blog
-                user={user}
-                blog={blog}
-                handleBlogUpdate={handleBlogUpdate}
-                handleRemove={handleRemove}
-              />
-            ) : (
-              <PageNotFound />
-            )
-          }
-        />
-        <Route
-          path="/login"
-          element={<LoginForm handleLoginForm={handleLoginForm} />}
-        />
+        <Route path="/blogs/:id" element={<Blog />} />
+        <Route path="/login" element={<LoginForm />} />
         <Route
           path="/"
           element={
             <ErrorBoundary>
-              <Blogs sortedBlogs={sortedBlogs} />{" "}
+              <Blogs />
             </ErrorBoundary>
           }
         />
