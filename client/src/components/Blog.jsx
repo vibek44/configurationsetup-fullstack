ce3d0abf952d4bfcb08d20d8d87f770320122e29
vibek44 <kshetrivibek@gmail.com>
2026-06-03 15:19:43 +0300

    allocating all functions needed for Form component using hook store instead of passing props

--- client/src/components/Blog.jsx
+++ client/src/components/Blog.jsx
@@ -1,30 +1,77 @@
-import { Link } from 'react-router-dom'
-import { Card, Typography,Stack,Button } from '@mui/material' 
+import { Link } from "react-router-dom";
+import { Card, Typography, Stack, Button } from "@mui/material";
+import { useBlog, useUser } from "../hooks/store";
+import { useMatch } from "react-router-dom";
+import PageNotFound from "./PageNotFound";
+import { useBlogActions } from "../hooks/store";
+import { useNavigate } from "react-router-dom";
 
+const Blog = () => {
+  const user = useUser();
+  const blogs = useBlog();
+  const { handleLikeUpdate, removeBlog } = useBlogActions();
+  const navigate = useNavigate();
+  const match = useMatch("/blogs/:id");
+  const id = match.params.id;
+  const blog = blogs.find((el) => el.id === id);
 
-const Blog = ({ user, blog ,handleBlogUpdate,handleRemove }) => {
-  const handleLikes = (blog) => {
-    const changedBlog={ ...blog,likes:blog.likes+1 }
-    handleBlogUpdate(changedBlog)
+  const handleLikes = async (blog) => {
+    const changedBlog = { ...blog, likes: blog.likes + 1 };
+    await handleLikeUpdate(changedBlog);
+  };
+
+  const handleRemove = async (blog) => {
+    const result = confirm(`Remove ${blog.title} by ${blog.author}`);
+    if (!result) return;
+    await removeBlog(blog.id);
+    navigate("/");
+  };
+
+  if (!blog) {
+    return <PageNotFound />;
   }
-
-  if (!blog)return null
-  return(
-    <Card  className='detail' style={{ marginTop:'1em',padding:'1em' }}>
-      <Stack spacing={1} >
-        <Typography variant='h5' > {blog.title} </Typography>
-        <Typography variant='subtitle1' > by {blog.author}</Typography>
-        <Link style={{color:'#0080FE'}} to={blog.url} target="_blank" rel='noreferer'  >{blog.url}</Link>
-        <Typography >
-          likes:{blog.likes} {user && <Button  variant='outlined' size='small' sx={{margin:'1em'}} onClick={() => handleLikes(blog)}>like</Button>}
-          {( user && (user.id===blog.user.id))  && <Button variant='outlined' size='small' color='error'  onClick={() => handleRemove(blog)}>remove</Button>}
-         </Typography>
-        <Typography variant='subtitle1 '> Created by {blog.user.userName} </Typography>
+  return (
+    <Card className="detail" style={{ marginTop: "1em", padding: "1em" }}>
+      <Stack spacing={1}>
+        <Typography variant="h5"> {blog.title} </Typography>
+        <Typography variant="subtitle1"> by {blog.author}</Typography>
+        <Link
+          style={{ color: "#0080FE" }}
+          to={blog.url}
+          target="_blank"
+          rel="noreferer"
+        >
+          {blog.url}
+        </Link>
+        <Typography>
+          likes:{blog.likes}{" "}
+          {user && (
+            <Button
+              variant="outlined"
+              size="small"
+              sx={{ margin: "1em" }}
+              onClick={() => handleLikes(blog)}
+            >
+              like
+            </Button>
+          )}
+          {user && user.id === blog.user.id && (
+            <Button
+              variant="outlined"
+              size="small"
+              color="error"
+              onClick={() => handleRemove(blog)}
+            >
+              remove
+            </Button>
+          )}
+        </Typography>
+        <Typography variant="subtitle1 ">
+          {" "}
+          Created by {blog.user.userName}{" "}
+        </Typography>
       </Stack>
     </Card>
-
-  )
-
-}
-export default Blog
-
+  );
+};
+export default Blog;
