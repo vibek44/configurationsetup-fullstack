514bfee682149319202dbe701631f01ef4a19fe3
vibek44 <kshetrivibek@gmail.com>
2026-06-03 15:28:35 +0300

    retrieving user,blogs vlaue funeeded for MenuLink component  using hook store

--- client/src/components/MenuLink.jsx
+++ client/src/components/MenuLink.jsx
@@ -1,36 +1,72 @@
-import { NavLink } from 'react-router-dom'
-import { Stack} from '@mui/material'
+import { NavLink } from "react-router-dom";
+import { Stack } from "@mui/material";
+import { useBlog, useUserActions } from "../hooks/store";
+import { useNavigate } from "react-router-dom";
+import { useUser } from "../hooks/store";
 
+const MenuLink = ({ sortedBlogs }) => {
+  const navigate = useNavigate();
+  const user = useUser();
+  const blogs = useBlog();
+  const { handleLogout } = useUserActions();
 
-const MenuLink = ({ user,handleLogout,sortedBlogs }) => {
-  const styleLink= ({isActive})=>({ 
-      textDecoration: isActive? '' :'none',
-      color: isActive? 'orange' :'white',
-      marginLeft:'1em'  
-    })
+  const logoutHandle = () => {
+    handleLogout();
+    navigate("/");
+  };
 
-  const styleStack={
-    alignItems:'center',
-    backgroundColor:'#0080FE',
-    height:'4em',
-    margin: '1em auto',
-    justifyContent:'space-between'
- }
-   
-  return(
-    <Stack   direction='row' style={styleStack}>
-        <Stack >
-        <NavLink style={{textDecoration:'none', marginLeft:'1em',color:'white',fontSize:'1.5em'} } to='/'>BlogApp</NavLink>
+  const styleLink = ({ isActive }) => ({
+    textDecoration: isActive ? "" : "none",
+    color: isActive ? "orange" : "white",
+    marginLeft: "1em",
+  });
+
+  const styleStack = {
+    alignItems: "center",
+    backgroundColor: "#0080FE",
+    height: "4em",
+    margin: "1em auto",
+    justifyContent: "space-between",
+  };
+
+  return (
+    <Stack direction="row" style={styleStack}>
+      <Stack>
+        <NavLink
+          style={{
+            textDecoration: "none",
+            marginLeft: "1em",
+            color: "white",
+            fontSize: "1.5em",
+          }}
+          to="/"
+        >
+          BlogApp
+        </NavLink>
+      </Stack>
+      {typeof blogs !== "undefined" && (
+        <Stack direction="row" spacing={1} sx={{ marginRight: "1em" }}>
+          <NavLink style={styleLink} to="/">
+            BLOGS
+          </NavLink>
+
+          {user && (
+            <NavLink style={styleLink} to="/create" caseSensitive>
+              NEW BLOG
+            </NavLink>
+          )}
+          {user ? (
+            <button style={{ height: "1.5em" }} onClick={logoutHandle}>
+              LOGOUT
+            </button>
+          ) : (
+            <NavLink style={styleLink} to="/login" caseSensitive>
+              LOGIN
+            </NavLink>
+          )}
         </Stack>
-        { typeof sortedBlogs!=='undefined' &&
-          <Stack  direction='row' spacing={1} sx={{marginRight:'1em'}} >
-          <NavLink  style={styleLink} to='/' >BLOGS</NavLink>
-        
-        {user && <NavLink style={styleLink} to='/create'  caseSensitive >NEW BLOG</NavLink>}
-        { user ? <button style={{height:'1.5em'}}  onClick={handleLogout}>LOGOUT</button>:<NavLink  style={styleLink} to='/login'   caseSensitive>LOGIN</NavLink>} 
-        </Stack>
-        }
+      )}
     </Stack>
-  )
-}
-export default MenuLink
+  );
+};
+export default MenuLink;
