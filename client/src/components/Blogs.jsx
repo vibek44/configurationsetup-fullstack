8b515b010919ff2f24c0e6b1bed896d89748c071
vibek44 <kshetrivibek@gmail.com>
2026-06-03 15:24:35 +0300

    retrieving blogs state   needed for Blogs  using hook useBlog from store  instead of passing value  as  props

--- client/src/components/Blogs.jsx
+++ client/src/components/Blogs.jsx
@@ -1,18 +1,31 @@
-import { Link } from 'react-router-dom'
+import { useEffect } from "react";
+import { Link } from "react-router-dom";
+import { useBlog } from "../hooks/store";
 
-const Blogs = ( { sortedBlogs } ) => {
+const Blogs = () => {
+  const blogs = useBlog();
+  const sortedBlogs = blogs
+    ? blogs.toSorted((a, b) => b.likes - a.likes)
+    : undefined;
 
-  if( typeof sortedBlogs ==='undefined')throw new Error('Try again and make a bug report') 
-   
-  return(
+  if (typeof sortedBlogs === "undefined")
+    throw new Error("Try again and make a bug report");
+
+  return (
     <div>
       <h3>Blogs</h3>
-      <ul className='briefly'>
-        {  sortedBlogs.map(blog => <li key={blog.id }> <Link to={`/blogs/${blog.id}`}>{blog.title} By {blog.author}</Link> </li> )
-        }
+      <ul className="briefly">
+        {sortedBlogs.map((blog) => (
+          <li key={blog.id}>
+            {" "}
+            <Link to={`/blogs/${blog.id}`}>
+              {blog.title} By {blog.author}
+            </Link>{" "}
+          </li>
+        ))}
       </ul>
     </div>
-  )
-}
+  );
+};
 
-export default Blogs
+export default Blogs;
