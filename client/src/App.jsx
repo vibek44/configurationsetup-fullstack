67d6a29da14922ba88b3c7f2f5b73ab1d60f3c59
vibek44 <kshetrivibek@gmail.com>
2026-06-04 13:41:35 +0300

    adding page navigation functionality during page reload using useMatch and useNavigate hook from react-router-dom

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,20 +10,27 @@ import Blog from "./components/Blog";
 import Togglable from "./components/Togglable";
 import LoginForm from "./components/LoginForm";
 import BlogForm from "./components/BlogForm";
+import Users from "./components/Users";
+import UserDetail from "./components/UserDetail";
 import { useBlogActions, useUserActions } from "./hooks/store";
+import { useUsersActions } from "./hooks/users-store";
 import blogService from "./services/blogs";
 import { getUser } from "./services/persistentUser";
 const App = () => {
   const { setUserStore } = useUserActions();
+  const { initializeUsers } = useUsersActions();
 
   const { initialize } = useBlogActions();
 
   const navigate = useNavigate();
 
   const match = useMatch("/blogs/:id");
+  const match2 = useMatch("/users/:id");
+  const match3 = useMatch("/users");
 
   useEffect(() => {
     initialize();
+    initializeUsers();
   }, []);
 
   useEffect(() => {
@@ -33,6 +40,10 @@ const App = () => {
       setUserStore(userLocal);
       if (match) {
         navigate(`/blogs/${match.params.id}`);
+      } else if (match2) {
+        navigate(`/users/${match2.params.id}`);
+      } else if (match3) {
+        navigate("/users");
       } else navigate("/");
     }
   }, []);
@@ -51,6 +62,8 @@ const App = () => {
           }
         />
         <Route path="/blogs/:id" element={<Blog />} />
+        <Route path="/users/:id" element={<UserDetail />} />
+        <Route path="/users" element={<Users />} />
         <Route path="/login" element={<LoginForm />} />
         <Route
           path="/"
