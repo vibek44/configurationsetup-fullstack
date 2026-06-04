fc22a305016352b11a774910cecfc3fbffb17d1a
vibek44 <kshetrivibek@gmail.com>
2026-06-04 13:24:36 +0300

    adding function createComment and exporting from module

--- client/src/services/blogs.js
+++ client/src/services/blogs.js
@@ -1,44 +1,54 @@
-import axios from 'axios'
-const baseUrl = '/api/blogs'
+import axios from "axios";
+const baseUrl = "/api/blogs";
 
-let token=null
+let token = null;
 const setToken = (newToken) => {
-  token=`Bearer ${newToken}`
-}
+  token = `Bearer ${newToken}`;
+};
 
-const getAll = async() => {
-  
-  const response = await axios.get(baseUrl)
-  return  response.data
-  
-  
-}
+const getAll = async () => {
+  const response = await axios.get(baseUrl);
+  return response.data;
+};
 
-const create = async(newBlog) => {
-  const config={
-    headers:{ Authorization:token }
-  }
-  const response=await axios.post(baseUrl, newBlog, config)
-  return response.data
+const create = async (newBlog) => {
+  const config = {
+    headers: { Authorization: token },
+  };
+  const response = await axios.post(baseUrl, newBlog, config);
+  return response.data;
+};
 
-}
+const update = async (updatedBlog) => {
+  const config = {
+    headers: { Authorization: token },
+  };
+  const response = await axios.put(
+    `${baseUrl}/${updatedBlog.id}`,
+    updatedBlog,
+    config
+  );
+  return response.data;
+};
 
-const update = async(updatedBlog) => {
-  const config={
-    headers:{ Authorization:token }
-  }
-  const response=await axios.put(`${baseUrl}/${updatedBlog.id}`, updatedBlog, config)
-  return response.data
-}
+const remove = async (id) => {
+  const config = {
+    headers: { Authorization: token },
+  };
+  await axios.delete(`${baseUrl}/${id}`, config);
+  return;
+};
 
-const remove = async(id) => {
-  const config={
-    headers:{ Authorization:token }
-  }
-  await axios.delete(`${baseUrl}/${id}`,config)
-  return
-}
+const createComment = async (comment, blogId) => {
+  const config = {
+    headers: { Authorization: token },
+  };
+  const response = await axios.post(
+    `${baseUrl}/${blogId}/comments`,
+    comment,
+    config
+  );
+  return response.data;
+};
 
-
-
-export default { getAll ,setToken,create,update,remove }
+export default { getAll, setToken, create, update, remove, createComment };
